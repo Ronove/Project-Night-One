45a551c7fb3380c0db0ff218f756953fe38445b8
nguillemot <nicolasg@uvic.ca>
2011-10-02 22:54:59 -0700

    separated tips.scripts from the footer, and also made it automatically create nscript.dat in the /export directory

--- compile.bat
+++ compile.bat
@@ -1,7 +1,26 @@
 @ECHO off
 
-ECHO Exporting scripts...
-tools\export_scripts\export_scripts.exe scripts.txt tools\export_scripts\compile_scripts.bat
+ECHO Exporting scripts from scripts.txt
+REM Creates compile_scripts.bat from scripts.txt in the root directory
+tools\export_scripts\export_scripts.exe scripts.txt tools\export_scripts\compile_scripts.bat>NUL
+ECHO Done.
+ECHO.
 
-ECHO Compiling scripts...
-tools\export_scripts\compile_scripts.bat>NUL
+ECHO Compiling scripts into one 0.txt
+REM create 0.txt from the generated compile_scripts.bat
+CALL tools\export_scripts\compile_scripts.bat>NUL
+ECHO Done.
+ECHO.
+
+ECHO Creating nscript.dat using nscmake
+ECHO Press any key to continue
+REM create nscript.dat using nscmake
+tools\nscr_sdk\nscmake.exe>NUL
+ECHO Done.
+ECHO.
+
+ECHO Copying nscript.dat into /export
+REM move the created nscript to the export directory
+MOVE nscript.dat export\>NUL
+ECHO Done.
+ECHO.
