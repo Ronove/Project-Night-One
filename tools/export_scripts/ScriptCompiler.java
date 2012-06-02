983b67f33076608c8ab951d3be62b2d200a30f64
nguillemot <nicolasg@uvic.ca>
2012-06-02 02:04:09 -0700

    switched 0.txt compilation to java because of some weird problems

---
+++ tools/export_scripts/ScriptCompiler.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+// Takes a list of script file names and creates 0.txt by putting them end to end
+class ScriptCompiler
+{
+	public static void main(String[] args) throws IOException
+	{
+		if(args.length != 1)
+		{
+			System.out.println("Usage: ScriptCompiler <scripts.txt>");
+			return;
+		}
+		
+		Scanner sc = new Scanner(new FileReader(args[0]));
+		PrintWriter pw = new PrintWriter(new FileOutputStream("0.txt"));
+			
+		while (sc.hasNextLine())
+		{
+			File f = new File(sc.nextLine());
+			System.out.println("Adding " + f.getPath() + " to 0.txt");
+			BufferedReader br = new BufferedReader(new FileReader(f.getPath()));
+			String line = br.readLine();
+			while (line != null)
+			{
+				pw.println(line);
+				line = br.readLine();
+			}
+			br.close();
+		}
+		
+		pw.close();
+		System.out.println("Done writing 0.txt");
+	}
+}
