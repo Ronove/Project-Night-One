c67962efc5df310bb4f6e8728a5b0b9e3c877626
nguillemot <nicolasg@uvic.ca>
2012-06-02 02:04:36 -0700

    now calls java program instead of batch script (commented out all the other old crap)

--- make_scripts.bat
+++ make_scripts.bat
@@ -1,13 +1,14 @@
 @ECHO off
 
-ECHO Exporting scripts from scripts.txt
+REM ECHO Exporting scripts from scripts.txt
 REM Creates compile_scripts.bat from scripts.txt in the root directory
-tools\export_scripts\export_scripts.exe scripts.txt tools\export_scripts\compile_scripts.bat
-ECHO.
+REM tools\export_scripts\export_scripts.exe scripts.txt tools\export_scripts\compile_scripts.bat
+REM ECHO.
 
 ECHO Compiling scripts into one 0.txt
 REM create 0.txt from the generated compile_scripts.bat
-CALL tools\export_scripts\compile_scripts.bat
+REM CALL tools\export_scripts\compile_scripts.bat
+java -cp tools\export_scripts\ ScriptCompiler scripts.txt
 ECHO Done.
 ECHO.
 
